c05483a7efa34ce33d32df845474ad66b40f9070
Didier Defrance <d2f.defrance@gmail.com>
2019-01-22 12:33:56 +0100

    avec class de style css et valeurs par defaut dans constructeur

--- src/app/emprunt/emprunt.component.ts
+++ src/app/emprunt/emprunt.component.ts
@@ -7,8 +7,14 @@ import { CalculEmprunt } from '../calcul-emprunt';
   styleUrls: ['./emprunt.component.css']
 })
 export class EmpruntComponent implements OnInit {
-  calculEmprunt: CalculEmprunt = new CalculEmprunt();
-  constructor() { }
+  calculEmprunt: CalculEmprunt;
+                       
+  constructor() {
+    this.calculEmprunt = new CalculEmprunt();
+    this.calculEmprunt.duree = 10;
+    this.calculEmprunt.taux = 3.0;
+    this.calculEmprunt.montant = 20000;
+   }
 
   calculerEmprunt(): void {
     this.calculEmprunt.calculer();
