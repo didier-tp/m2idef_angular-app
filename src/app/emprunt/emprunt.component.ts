a0761d3c1eae123107b568eb32a9256687e6872d
Didier Defrance <d2f.defrance@gmail.com>
2019-01-22 14:09:06 +0100

    Object.assign in comment

--- src/app/emprunt/emprunt.component.ts
+++ src/app/emprunt/emprunt.component.ts
@@ -10,10 +10,25 @@ export class EmpruntComponent implements OnInit {
   calculEmprunt: CalculEmprunt;
                        
   constructor() {
+    
     this.calculEmprunt = new CalculEmprunt();
     this.calculEmprunt.duree = 10;
     this.calculEmprunt.taux = 3.0;
     this.calculEmprunt.montant = 20000;
+    //OK: method calculer() ET structure ok
+    /*
+   this.calculEmprunt = {
+      montant : 20000,
+      duree : 10,
+      taux: 3.0
+   }; MISSING .mensualite and .calculer()
+   */
+  /*
+  this.calculEmprunt =
+  Object.assign(new CalculEmprunt(),
+                { montant: 20000,  duree: 10,  taux: 3.0 });
+  //OK
+  */
    }
 
   calculerEmprunt(): void {
