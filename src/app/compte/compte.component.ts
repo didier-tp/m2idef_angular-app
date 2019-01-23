0af0e21207fb6752b2868a824b0438ec6580d162
Didier Defrance <d2f.defrance@gmail.com>
2019-01-23 11:51:13 +0100

    s

--- src/app/compte/compte.component.ts
+++ src/app/compte/compte.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Compte } from '../compte';
+import { HttpClient , HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-compte',
@@ -7,19 +8,20 @@ import { Compte } from '../compte';
   styleUrls: ['./compte.component.css']
 })
 export class CompteComponent implements OnInit {
-
   compte: Compte = new Compte();
   // compte : object = { };
   // compte: object = { numero: null , label : "c" , solde : 50.0};
-
   message: string = "";
+  private _headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+  constructor(private http: HttpClient) { }
 
   ajouterCompte(): void {
     this.message = "compte=" + JSON.stringify(this.compte);
     //+ futur appel WS REST
   }
 
-  constructor() { }
+  
 
   ngOnInit() {
   }
