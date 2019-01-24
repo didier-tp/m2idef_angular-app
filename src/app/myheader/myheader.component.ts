9121f09b47a597b0602494033a218c12301defed
Didier Defrance <d2f.defrance@gmail.com>
2019-01-24 16:32:36 +0100

    avec @Input sur title de myheader

--- src/app/myheader/myheader.component.ts
+++ src/app/myheader/myheader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-myheader',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./myheader.component.css']
 })
 export class MyheaderComponent implements OnInit {
+
+  @Input()
+  title : string ="titre par defaut";
 
   constructor() { }
 
