5433c5bb348c8e4cf8b91851a26919e85b470ed2
Didier Defrance <d2f.defrance@gmail.com>
2019-01-25 11:19:06 +0100

    navbar responsive

--- src/app/myheader/myheader.component.ts
+++ src/app/myheader/myheader.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { MenuDefinition } from 'src/bs-util/data/MenuDefinition';
 
 @Component({
   selector: 'app-myheader',
@@ -9,6 +10,20 @@ export class MyheaderComponent implements OnInit {
 
   @Input()
   title : string ="titre par defaut";
+
+  myMenuDefs: MenuDefinition[] = [
+    {
+      label: "MyDropDownMenu1",
+      children: [
+        { label: "menu-item-basic", path: "basic" },
+        { label: "menu-item-compte", path: "compte" },
+        { divider: true },
+        { label: "menu-item-basic", path: "basic" }
+      ]
+    },
+    { label: "menu-item-basic", path: "basic" },
+    { label: "menu-item-compte", path: "compte" }
+  ];
 
   constructor() { }
 
